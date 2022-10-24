2bfae90b88ce5926b9093f438cc95224789d3297
Jacob Alexander Thompson <jacobalthompson@gmail.com>
2022-10-24 03:19:42 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name = "pong-atari",
-    version = "1.1.0",
+    version = "1.1.1",
     description = "Pong written using pygame",
     long_description = long_description,
     long_description_content_type = "text/markdown",
