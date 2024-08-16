5cc68860bfd758ae436dcaf55a121ccdb1056ce7
Jacob Alexander Thompson <jacobalthompson@gmail.com>
2024-08-16 03:59:01 -0700

    test necessary pygame modules individually
    unittest modules seem to be unable to access audio devices, so pygame.mixer module can not be tested in this manner

--- tests/test_pong.py
+++ tests/test_pong.py
@@ -4,8 +4,21 @@ import pygame
 
 class TestPong(unittest.TestCase):
     def test_pygame_works(self):
-        pygame.init()
-        self.assertTrue(pygame.get_init())
+        pygame.display.init()
+        pygame.font.init()
+        #pygame.mixer.init()
+
+        self.assertTrue(pygame.display.get_init())
+        self.assertTrue(pygame.font.get_init())
+        #self.assertTrue(pygame.mixer.get_init() != None)
+
+        pygame.display.quit()
+        pygame.font.quit()
+        #pygame.mixer.quit()
+
+        # unittest modules seem to be unable to access
+        ## audio devices, so pygame.mixer module
+        ## can not be tested in this manner
 
 if __name__ == '__main__':
     unittest.main()
